44b491567c8ec3c843d615ebadc49ab306ae225c
Marcelo <marcelopugasjunior@gmail.com>
2023-01-13 00:25:33 -0300

    Add svg suport

--- lib/src/pages/splash/splash_page.dart
+++ lib/src/pages/splash/splash_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:invoice_notification/src/theme/colors.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: InvoiceColors.silver,
+      child: Center(
+          child: SvgPicture.asset(
+        width: MediaQuery.of(context).size.width * 0.7,
+        'assets/icons/new_notification.svg',
+      )),
+    );
   }
 }
